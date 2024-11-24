b088e40a8b127a6ed90dc3dca0bdf30b53f36e59
Ronado Vargas <ronaldovh99@gmail.com>
2024-11-24 13:40:58 -0500

    Add MusicaWebServie Tabla MusicaEntity MusicaResponse

---
+++ SleepSoundsAPI/Data/Modelo/MusicaEntity.cs
@@ -0,0 +1,10 @@
+namespace SleepSoundsAPI.Data.Modelo;
+
+public class MusicaEntity
+{
+    public int Id { get; set; }
+    public string? Artista { get; set; }
+    public string? Titulo { get; set; }
+    public string? Album { get; set; }
+    public string? Categoria { get; set; }
+}
